a23a1662ee85ed3f8dab570835e1513f4e4a69e9
jose <joseakd136@gmail.com>
2023-12-03 22:05:50 -0300

    Agregar archivos de proyecto.

---
+++ ReTurnoWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReTurnoWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
